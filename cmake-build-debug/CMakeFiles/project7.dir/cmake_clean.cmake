e4a239f51af94d2db902ddbb0fe1cadf1f244132
Alexa-Brown <browam18@wfu.edu>
2019-11-07 15:54:59 -0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/project7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project7.dir/main.cpp.obj"
+  "project7.pdb"
+  "project7.exe"
+  "project7.exe.manifest"
+  "libproject7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

375f9d2114ad0916e0fe0ca845c10dbcf10f20d0
Alexa-Brown <browam18@wfu.edu>
2019-11-07 16:43:10 -0500

    added the > and < operator overloads

--- Data.cpp
+++ Data.cpp
@@ -1,5 +1,29 @@
-//
-// Created by alexa on 11/7/2019.
-//
 
 #include "Data.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+bool Data:: operator > (Data d){
+    if (this->happinessRank < d.happinessRank){
+        return false;
+    }
+    else if (this->happinessRank > d.happinessRank){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+bool Data:: operator < (Data d){
+    if (this->happinessRank > d.happinessRank){
+        return false;
+    }
+    else if (this->happinessRank < d.happinessRank){
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
